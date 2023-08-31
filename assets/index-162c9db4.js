39477ac51a90a01b35809997a1a494ee2f8ae376
qiangzhili <1205131727@qq.com>
2023-08-31 14:42:29 +0800

    增加分红代币模板

--- assets/index-162c9db4.js
+++
@@ -1,1 +0,0 @@
-import{K as t}from"./index-645b9a47.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
---
+++ assets/index-a47ffde4.js
@@ -0,0 +1,1 @@
+import{K as t}from"./index-0f0f7870.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
