04215c46911577f98b063701be4d508279de1cea
caitongxue <675398066@qq.com>
2023-09-05 10:49:14 +0800

    add bsc dividend fee

--- assets/index-71c2019f.js
+++
@@ -1,1 +0,0 @@
-import{K as t}from"./index-5bf2b147.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
---
+++ assets/index-e83a6744.js
@@ -0,0 +1,1 @@
+import{K as t}from"./index-8467ff0d.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
