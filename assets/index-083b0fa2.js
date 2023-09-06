36ff6155be70ab5d02971c8114ecf98fae1cca10
qiangzhili <1205131727@qq.com>
2023-09-06 18:33:28 +0800

    core链合约更新V2

--- assets/index-083b0fa2.js
+++
@@ -1,1 +0,0 @@
-import{K as t}from"./index-923fe510.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
---
+++ assets/index-6bdbde7d.js
@@ -0,0 +1,1 @@
+import{N as t}from"./index-fbe4e919.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
