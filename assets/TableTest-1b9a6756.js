18f2f49770072618cd9eb2a18677039d68a2c242
caitongxue <675398066@qq.com>
2023-09-25 17:33:24 +0800

    增加测试网2

---
+++ assets/TableTest-1b9a6756.js
@@ -0,0 +1,1 @@
+import{o as e,c as t}from"./index-a6966cbe.js";const s={__name:"TableTest",setup(a){return(c,o)=>(e(),t("div"))}};export{s as default};
---
+++ assets/index-d3e8bb55.js
@@ -0,0 +1,1 @@
+import{a0 as t}from"./index-a6966cbe.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
