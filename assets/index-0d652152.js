ded90f041088fa5863c745a9c5f15faa95f4c389
qiangzhili <1205131727@qq.com>
2023-11-13 12:03:58 +0800

    完善锁的权限转移

--- assets/index-0d652152.js
+++
@@ -1,1 +0,0 @@
-import{a1 as t}from"./index-1a6d1bbc.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
---
+++ assets/index-aaece16c.js
@@ -0,0 +1,1 @@
+import{a1 as t}from"./index-00af62d0.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
