c8c7715aa14551a0fb9c11a77c09df6e81eda2ab
weizhuang <675398066@qq.com>
2024-05-29 00:55:53 +0800

    update ethers

--- assets/index-2b46ef08.js
+++
@@ -1,1 +0,0 @@
-import{a1 as t}from"./index-648556e7.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
---
+++ assets/index-ab5dacea.js
@@ -0,0 +1,1 @@
+import{a1 as t}from"./index-a94cb140.js";const e=t("mt",{state:()=>({transferringList:[]}),actions:{}});export{e as u};
